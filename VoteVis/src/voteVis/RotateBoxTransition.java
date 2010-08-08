5173db4122e42608ed6ae7ff7fd572c5852d33b3
Patrick Tierney <patrick.l.tierney@gmail.com>
2010-08-07 18:32:59 -0700

    Working on rotation transition, it's still buggy

--- VoteVis/src/voteVis/RotateBoxTransition.java
+++ VoteVis/src/voteVis/RotateBoxTransition.java
@@ -5,7 +5,7 @@ import processing.core.*;
 public class RotateBoxTransition extends BoxTransition {
 	private Axis axis_;
 	private float rotate_amount_;
-	private static float SEAM = 20.0f; // the amount of space between two images
+	private static float SEAM = 0.0f; // the amount of space between two images
 	private static float ROTATE_SPEED = 0.03f; // maybe make this time based?
 	private int pos_x_;
 	private int pos_y_;
@@ -20,7 +20,7 @@ public class RotateBoxTransition extends BoxTransition {
 	public void load_boxes(Box b1, Box b2) {
 		super.load_boxes(b1, b2);
 
-		rotate_amount_ = 0.0f;
+		rotate_amount_ = -PApplet.PI;
 		
 		pos_x_ = (int) b1.x();
 		pos_y_ = (int) b1.y();
@@ -29,12 +29,14 @@ public class RotateBoxTransition extends BoxTransition {
 	public void update() {
 		if (!transitioning_)
 			return;
-		
-		rotate_amount_ += ROTATE_SPEED;
+		if (VoteVisApp.instance().key == 'a') {
+			rotate_amount_ += ROTATE_SPEED;
+			PApplet.println(rotate_amount_);
+		}
 	}
 
 	public void draw() {
-		if (!transitioning_)
+		if (false && !transitioning_)
 			return;
 		
 		PApplet p = VoteVisApp.instance();
@@ -46,23 +48,30 @@ public class RotateBoxTransition extends BoxTransition {
 			else if (axis_ == Axis.HORIZONTAL)
 				p.rotateX(rotate_amount_);
 			
-			p.beginShape();
-				p.texture(start_image_);
-				p.vertex(-h_start_width_, -h_start_height_, SEAM, 0, 0);
-				p.vertex(-h_start_width_, h_start_height_, SEAM, 0, start_height_);
-				p.vertex(h_start_width_, h_start_height_, SEAM, start_width_, start_height_);
-				p.vertex(h_start_width_, -h_start_height_, SEAM, start_width_, 0);
-			p.endShape(PApplet.CLOSE);
-			
-			p.beginShape();
-				p.texture(end_image_);
-				p.vertex(-h_end_width_, -h_end_height_, -SEAM, 0, 0);
-				p.vertex(h_end_width_, -h_end_height_, -SEAM, end_width_, 0);
-				p.vertex(h_end_width_, h_end_height_, -SEAM, end_width_, end_height_);
-				p.vertex(-h_end_width_, h_end_height_, -SEAM, 0, end_height_);
-			p.endShape();
+			if (true || rotate_amount_ < 0) {
+				p.beginShape();
+					p.texture(start_image_);
+					p.vertex(-h_start_width_, -h_start_height_, 0, 0);
+					p.vertex(-h_start_width_, h_start_height_, 0, start_height_);
+					p.vertex(h_start_width_, h_start_height_,  start_width_, start_height_);
+					p.vertex(h_start_width_, -h_start_height_, start_width_, 0);
+				p.endShape();
+			} else {
+				p.beginShape();
+					p.texture(end_image_);
+					p.vertex(-h_end_width_, -h_end_height_,  0, 0);
+					p.vertex(-h_end_width_, h_end_height_,  0, start_height_);
+					p.vertex(h_end_width_, h_end_height_,  end_width_, end_height_);
+					p.vertex(h_end_width_, -h_end_height_,  start_width_, 0);
+				p.endShape();
+			}
 			
 		p.popMatrix();
+		
+		if (rotate_amount_ > PApplet.PI / 2) {
+			rotate_amount_ = PApplet.PI / 2;
+			transitioning_ = false;
+		}
 	}
 
 	public enum Axis {
